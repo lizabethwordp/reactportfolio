0a7c10a42a7f4451f064c78686eab825305b9931
lizabeth <52053436+lizabethwordp@users.noreply.github.com>
2024-06-23 12:16:09 +0100

    Update About.js

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -24,7 +24,7 @@ const About = () => {
               <article className='about__card'>
                 <FaAward className='about__icon'/>
                 <h5>Experience</h5>
-                <small>3 year working experience</small>
+                <small>3 years working experience</small>
               </article>
 
               <article className='about__card'>
