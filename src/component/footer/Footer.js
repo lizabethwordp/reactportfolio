7d1eb1dc26397c66ded82f50495d6387a558a130
lizabeth <52053436+lizabethwordp@users.noreply.github.com>
2024-06-23 12:00:00 +0100

    Update Footer.js

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,35 +1,13 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import './footer.css';
 
 const Footer = () => {
   return (
-
-   
-       
     <footer>
-      <hr
-        style={{
-          background: 'gray',
-          height: '0.7px',
-          
-          left: '1rem',
-          right: '5rem',
-          marginTop: '7rem',
-          marginLeft: '10rem',
-          marginRight: '10rem'
-        }}
-      />
-
+      <hr />
       <small>&copy; Elizabeth. All rights reserved.</small>
-    
-    
-      
     </footer>
+  );
+};
 
-    
-      
-      
-  )
-}
-
-export default Footer
+export default Footer;
